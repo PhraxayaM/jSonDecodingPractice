181d70ccc0a1ab364cf68040e44babb009af951f
MattHew Phraxayavong <matthewphraxayavong@matthews-macbook-pro.local>
2019-02-08 18:13:03 -0800

    Initial Commit

---
+++ jsonPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jsonPractice
+//
+//  Created by MattHew Phraxayavong on 2/8/19.
+//  Copyright © 2019 MattHew Phraxayavong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
