c256924b1357f486b558a7349616f153b5822037
MattHew Phraxayavong <matthewphraxayavong@MattHews-MacBook-Pro.local>
2019-02-11 07:20:48 -0800

    added tableview programmatically

---
+++ jsonPractice/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  jsonPractice
+//
+//  Created by MattHew Phraxayavong on 2/8/19.
+//  Copyright © 2019 MattHew Phraxayavong. All rights reserved.
+//
+
+import Foundation
--- jsonPractice/ViewController.swift
+++ jsonPractice/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.red
+        navigationItem.title = "Festivals"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: String?) -> String? {
+        let path = Bundle.main.path(forResource: "festival", ofType: ".json")
+        if let path = path {
+            let url = URL(fileURLWithPath: path)
+            
+        }
+        return path
     }
 
 
